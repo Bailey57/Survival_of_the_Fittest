4a4fee8d7ade69a59a323ac74cefc7f7ce311c04
Bailey <77756589+Bailey57@users.noreply.github.com>
2023-02-15 03:34:45 -0500

    Added exit button

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -11,16 +11,5 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
